ef49dc6e4d6c20d9b491607e517fa6a9c7b248b8
Ann-Marie Greif <annx9854@stud.kea.dk>
2019-03-25 17:19:19 +0100

    3 sektioner færdig

--- script.js
+++ script.js
@@ -1,40 +1,61 @@
-     document.addEventListener("DOMContentLoaded", hentJson);
-     let valueArray = [];
-     let offsetArray = [0];
-     let omkreds = 200 * Math.PI;
-     let myData;
-     async function hentJson() {
-         let myJson = await fetch("data.json");
-         myData = await myJson.json();
-         //console.log(myData);
-         lavArray();
-     }
+document.addEventListener("DOMContentLoaded", hentJson);
+let valueArray = [];
+let offsetArray = [0];
+let omkreds = 200 * Math.PI;
+let myData;
+async function hentJson() {
+    let myJson = await fetch("data.json");
+    myData = await myJson.json();
+    //console.log(myData);
+    lavArray();
+}
 
-     function lavArray() {
-         myData.forEach(data => {
-             offsetArray.push(data.value + offsetArray[offsetArray.length - 1]);
-             valueArray.push(data.value);
-         })
-         //console.log(valueArray);
-         //console.log(offsetArray);
-         animer();
-     }
 
-     function animer() {
-         document.querySelectorAll(".piechart circle").forEach((pie, i) => {
-             pie.style.strokeDasharray = valueArray[i] + " " + omkreds;
-             pie.style.strokeDashoffset = -offsetArray[i];
-             pie.setAttribute("data-value", valueArray[i]);
-             // console.log(pie);
-         });
-         // document.querySelector(".resultat").innerHTML = `<p>${46} % </p></div>`;
-     }
-     //        document.querySelector(".piechart").addEventListener("mouseover", e => {
-     //            let valgt = e.target.getAttribute("data-value");
-     //
-     //            if (valgt) {
-     //                console.log(valgt);
-     //                document.querySelector(".resultat").innerHTML = `<div class><i>${46} % </i></div>`;
-     //
-     //            }
-     //        });
+function lavArray() {
+    myData.forEach(data => {
+        offsetArray.push(data.value + offsetArray[offsetArray.length - 1]);
+        valueArray.push(data.value);
+    })
+    //console.log(valueArray);
+    //console.log(offsetArray);
+    animer();
+}
+
+function animer() {
+    document.querySelectorAll(".piechart circle").forEach((pie, i) => {
+        pie.style.strokeDasharray = valueArray[i] + " " + omkreds;
+        pie.style.strokeDashoffset = -offsetArray[i];
+        pie.setAttribute("data-value", valueArray[i]);
+        // console.log(pie);
+    });
+    // document.querySelector(".resultat").innerHTML = `<p>${46} % </p></div>`;
+}
+//        document.querySelector(".piechart").addEventListener("mouseover", e => {
+//            let valgt = e.target.getAttribute("data-value");
+//
+//            if (valgt) {
+//                console.log(valgt);
+//                document.querySelector(".resultat").innerHTML = `<div class><i>${46} % </i></div>`;
+//
+//            }
+//        });
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("siden vises!")
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+}
+
+function toggleMenu() {
+    document.querySelector("#mobile").classList.toggle("hidden");
+
+
+    let erSkjult = document.querySelector("#mobile").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").innerHTML = "&times;";
+    }
+}
